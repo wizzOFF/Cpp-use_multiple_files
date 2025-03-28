5724550376e63c0f4c4ab9d3dcca4069a8e04208
WizzOFF <maher_off@proton.me>
2025-03-28 23:14:18 +0100

    math.cpp: function ajouterDeux

--- math.cpp
+++ math.cpp
@@ -1,2 +1,11 @@
 
 #include "math.hpp"
+#include <iostream>
+using namespace std;
+
+int ajouteDeux(int nombreRecu)
+{
+    int valeur(nombreRecu + 2);
+
+    return valeur;
+}
--- math.hpp
+++ math.hpp
@@ -2,4 +2,6 @@
 #ifndef MATH_HPP_INCLUDED
 #define MATH_HPP_INCLUDED
 
+int ajouteDeux(int nombreRecu);
+
 #endif // MATH_HPP_INCLUDED
