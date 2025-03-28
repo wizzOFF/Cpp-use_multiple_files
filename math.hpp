ab72611ddd4015bab3adb483b5a6dcd56bfa8488
WizzOFF <maher_off@proton.me>
2025-03-28 23:24:13 +0100

    math.hpp: comment on functions

--- math.hpp
+++ math.hpp
@@ -2,6 +2,11 @@
 #ifndef MATH_HPP_INCLUDED
 #define MATH_HPP_INCLUDED
 
+/*
+ * Fonction qui ajoute 2 au nombre reçu en argument
+ * - nombreRecu : Le nombre auquel la fonction ajoute 2
+ * Valeur retournée : nombreRecu + 2
+ */
 int ajouteDeux(int nombreRecu);
 
 #endif // MATH_HPP_INCLUDED
