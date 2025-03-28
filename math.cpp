aa1293a90fd715ab7beb3b9be5d098ebbf315dd8
WizzOFF <maher_off@proton.me>
2025-03-28 23:17:03 +0100

    math.cpp: function main

--- math.cpp
+++ math.cpp
@@ -9,3 +9,15 @@ int ajouteDeux(int nombreRecu)
 
     return valeur;
 }
+
+int main()
+{
+    int a(2),b(2);
+    cout << "Valeur de a : " << a << endl;
+    cout << "Valeur de b : " << b << endl;
+    b = ajouteDeux(a);                     //Appel de la fonction
+    cout << "Valeur de a : " << a << endl;
+    cout << "Valeur de b : " << b << endl;
+
+    return 0;
+}
